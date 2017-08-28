1d173a021c555d1b7c05e9145d98d2a94bbec072
zizon <ZhiZhongQiu@sf-express.com>
2017-08-28 18:11:12 +0800

    add hive query planer

---
+++ src/main/java/com/sf/misc/hive/HiveQueryPlaner.java
@@ -0,0 +1,216 @@
+package com.sf.misc.hive;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hive.ql.parse.ASTNode;
+import org.apache.hadoop.hive.ql.parse.HiveParser;
+import org.apache.hadoop.hive.ql.parse.ParseDriver;
+import org.apache.hadoop.hive.ql.parse.ParseException;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class HiveQueryPlaner {
+
+    private static final Log LOGGER = LogFactory.getLog(HiveQueryPlaner.class);
+
+    public static enum RejectReason {
+        None, ParseError;
+    }
+
+    public static class PlanGenerationFailException extends RuntimeException {
+        public PlanGenerationFailException(String message, Throwable throwable) {
+            super(message, throwable);
+        }
+    }
+
+    public static interface FetchPlan {
+    }
+
+    public static class VirtualFetchPlan implements FetchPlan {
+        public VirtualFetchPlan(String name, List<String> schema, List<List<String>> values) {
+
+        }
+    }
+
+
+    protected static final String DEFAULT_DATABASE = "_default_database_";
+
+    protected String query;
+    protected ASTNode root;
+    protected Exception parse_excetion;
+
+    protected ConcurrentMap<String, FetchPlan> fetch_plans;
+
+    public HiveQueryPlaner(String query) throws PlanGenerationFailException {
+        this.query = query;
+        this.parse_excetion = null;
+        try {
+            this.root = this.trim(new ParseDriver().parse(query));
+        } catch (ParseException e) {
+            throw new PlanGenerationFailException("fail to parse query:" + query, e);
+        }
+
+        this.fetch_plans = new ConcurrentHashMap<>();
+    }
+
+    public FetchPlan genQueryPlan() {
+        if (this.root.getType() != HiveParser.TOK_QUERY) {
+            // skip not query statement
+            return null;
+        }
+
+        this.genQueryPlan(this.root);
+        return null;
+    }
+
+
+    public Exception exception() {
+        return this.parse_excetion;
+    }
+
+    protected ASTNode trim(ASTNode node) {
+        List<ASTNode> new_nodes = new LinkedList<>();
+        for (int i = node.getChildCount() - 1; i >= 0; i--) {
+            ASTNode child = (ASTNode) node.getChild(i);
+            switch (child.getType()) {
+                case HiveParser.EOF:
+                    node.deleteChild(i);
+                    break;
+                default:
+                    // drill down and replace
+                    node.replaceChildren(i, i, trim(child));
+            }
+        }
+
+        if (node.getToken() == null && node.getChildCount() == 1) {
+            node = (ASTNode) node.getChild(0);
+        }
+
+        return node;
+    }
+
+    protected void consumeIfExists(ASTNode node, int child_token_type, Consumer<ASTNode> consumer) {
+        ASTNode candidate = (ASTNode) node.getFirstChildWithType(child_token_type);
+        if (candidate != null) {
+            consumer.accept(candidate);
+            node.deleteChild(candidate.getChildIndex());
+        }
+    }
+
+    protected FetchPlan genQueryPlan(ASTNode node) {
+        // process cte first
+        consumeIfExists(node, HiveParser.TOK_CTE, this::genCTEPlan);
+
+        // process query
+        consumeIfExists(node, HiveParser.TOK_QUERY, this::genQueryPlan);
+
+        // process from
+        consumeIfExists(node, HiveParser.TOK_FROM, this::genFromPlan);
+
+        //todo
+        return null;
+    }
+
+    protected void genCTEPlan(ASTNode node) {
+        ASTNode query = (ASTNode) node.getFirstChildWithType(HiveParser.TOK_SUBQUERY);
+
+        // gen plan
+        FetchPlan plan = this.genQueryPlan((ASTNode) query.getChild(0));
+
+        //todo
+        //this.fetch_plans.put(query.getChild(1).getText().toLowerCase(), plan);
+    }
+
+    protected void genFromPlan(ASTNode node) {
+        this.consumeIfExists(node,HiveParser.TOK_VIRTUAL_TABLE,this::genVirtualTablePlan);
+
+        this.consumeIfExists(node,HiveParser.TOK_JOIN,this::genJoinPlan);
+    }
+
+    protected void genVirtualTablePlan(ASTNode node) {
+        // find table name
+        ASTNode virtual_table = (ASTNode) node.getFirstChildWithType(HiveParser.TOK_VIRTUAL_TABREF);
+        String table_name = virtual_table.getFirstChildWithType(HiveParser.TOK_TABNAME).getChild(0).getText().toLowerCase();
+
+        // columns
+        ASTNode columns = (ASTNode) virtual_table.getFirstChildWithType(HiveParser.TOK_COL_NAME);
+        List<String> schema = columns.getChildren().stream().map(
+                (child) -> ((ASTNode) child).getText() //
+        ).collect(Collectors.toList());
+
+        // values
+        ASTNode values = (ASTNode) node.getFirstChildWithType(HiveParser.TOK_VALUES_TABLE);
+        List<List<String>> table_vlaues =values.getChildren().stream().map(
+                (child) -> child.getChildren().stream().map(
+                        (value_node) -> ((ASTNode)value_node).getText()
+                ).collect(Collectors.toList())
+        ).collect(Collectors.toList());
+
+        this.fetch_plans.put(table_name,new VirtualFetchPlan(table_name,schema,table_vlaues));
+    }
+
+    protected  void genJoinPlan(ASTNode node){
+        //todo
+        LOGGER.info(node.toStringTree());
+    }
+
+    @Override
+    public String toString() {
+        return this.root != null ? this.root.toStringTree() : "None";
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", ".");
+        String meta_uri = "thrift://10.202.77.200:9083";
+        String query = "select * from  a join b";
+        //String query = "with c as  (select * from x  Y) select * from (select * from b) a join (select * from values (1,2,3) as z (e,f,g)) d on a.c = d.c where c!=null";
+
+        try {
+            /*
+            //LOGGER.info(Thread.currentThread().getContextClassLoader().getResources(".").nextElement().toString());
+            HiveConf configuration = new HiveConf();
+
+            // guide meta store location
+            configuration.set("hive.metastore.uris", meta_uri);
+
+            // overwrite ugi.
+            // workaround for windows
+            configuration.setClass(CommonConfigurationKeys.HADOOP_SECURITY_GROUP_MAPPING, HiveMetaSyncer.AnyGroupMappingServiceProvider.class, GroupMappingServiceProvider.class);
+            UserGroupInformation.setConfiguration(configuration);
+
+
+            //configuration.set("hive.exec.scratchdir", Thread.currentThread().getContextClassLoader().getResources(".").nextElement().toString());
+            SessionState.start(configuration);
+            //SessionState.start(configuration);
+
+            //SessionState.start(configuration);
+            //SessionState.start(SessionState.get());
+
+            Context context = new Context(configuration);
+            ParseDriver driver = new ParseDriver();
+            ASTNode ast = driver.parse(query, context);
+            LOGGER.info(ast);
+             */
+            HiveQueryPlaner planer = new HiveQueryPlaner(query);
+            planer.genQueryPlan();
+
+
+            //LOGGER.info(((ASTNode) root.getChild(1)).getToken() == null);
+            //LOGGER.info(((ASTNode)root.getChild(1)).getToken().getClass());
+
+
+            // is query?
+            LOGGER.info(planer);
+
+        } catch (Exception e) {
+            LOGGER.error("unexpected exception", e);
+        }
+
+
+    }
+}
--- src/main/java/com/sf/misc/jstorm/JStromMetricKafkaUploader.java
+++ src/main/java/com/sf/misc/jstorm/JStromMetricKafkaUploader.java
@@ -2,9 +2,8 @@ package com.sf.misc.jstorm;
 
 import backtype.storm.Config;
 import backtype.storm.generated.MetricInfo;
-import backtype.storm.generated.Nimbus;
+import backtype.storm.generated.MetricSnapshot;
 import backtype.storm.generated.TopologyMetric;
-import backtype.storm.utils.NimbusClient;
 import backtype.storm.utils.Utils;
 import com.alibaba.jstorm.daemon.nimbus.NimbusData;
 import com.alibaba.jstorm.daemon.nimbus.metric.ClusterMetricsContext;
@@ -17,25 +16,57 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.CreateTopicsOptions;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.TopicExistsException;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
 public class JStromMetricKafkaUploader extends BaseMetricUploaderWithFlowControl implements MetricUploader {
 
     private static final Log LOGGER = LogFactory.getLog(JStromMetricKafkaUploader.class);
 
+    public static final String METRIC_KAFKA_TOPIC = "jstorm-metrics";
+
     protected ClusterMetricsContext metric_context;
     protected Gson gson;
+    protected Future<Boolean> topic_ok;
+    protected Future<KafkaProducer<String, String>> producer;
+
 
     @Override
     public void init(NimbusData nimbus_data) throws Exception {
         LOGGER.info("initialized metric uploader");
-        this.metric_context = new ClusterMetricsContext(nimbus_data);
         this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.metric_context = new ClusterMetricsContext(nimbus_data);
+
+        this.topic_ok = this.async(() -> {
+            try (AdminClient admin = AdminClient.create(kafkaConfig())) {
+                admin.createTopics(
+                        Collections.singleton(new NewTopic(METRIC_KAFKA_TOPIC, 10, (short) 1)),
+                        new CreateTopicsOptions()
+                ).all().get();
+                return true;
+            } catch (TopicExistsException e) {
+                return true;
+            } catch (Throwable throwable) {
+                LOGGER.error("fail to ensure topic:" + METRIC_KAFKA_TOPIC, throwable);
+                return false;
+            }
+        });
+
+        this.producer = this.metric_context.getNimbusData().getScheduExec().submit(() -> {
+            return new KafkaProducer<String, String>(this.kafkaConfig());
+        });
     }
 
     @Override
@@ -45,6 +76,13 @@ public class JStromMetricKafkaUploader extends BaseMetricUploaderWithFlowControl
         // help gc
         this.metric_context = null;
         this.gson = null;
+        try {
+            this.producer.get().close();
+        } catch (Exception exception) {
+            LOGGER.warn("unexpected exception when closing producer");
+        } finally {
+            this.producer = null;
+        }
     }
 
     @Override
@@ -77,14 +115,20 @@ public class JStromMetricKafkaUploader extends BaseMetricUploaderWithFlowControl
 
     @Override
     public boolean sendEvent(String cluster_name, MetricEvent event) {
-        //no particular events that are interested in
+        // no particular events that are interested in
         // note： if handle all events here, it may cause a continuously
         // pushing behavior eg. invoking this sendEvent with identical parameters
         return true;
     }
 
-    protected Future<?> upload(String cluster_name, String toplogy_id, Map<String, Object> metric_summary, int metric_cache_index) {
-        return ForkJoinPool.commonPool().submit(() -> {
+    protected <T> Future<T> async(Callable<T> runnable) {
+        return this.metric_context.getNimbusData().getScheduExec().submit(runnable);
+    }
+
+    protected Future<Boolean> upload(String cluster_name, String toplogy_id, Map<String, Object> metric_summary, int metric_cache_index) {
+        // reuse schedule thread pool in nimbus data.
+        return this.async(() -> {
+            boolean ok = false;
             try {
                 // get metric and its meta info
                 TopologyMetric metric = metric_context.getMetricDataFromCache(metric_cache_index);
@@ -99,19 +143,75 @@ public class JStromMetricKafkaUploader extends BaseMetricUploaderWithFlowControl
                 //push to kafka
                 String json = this.gson.toJson(root);
                 LOGGER.info("pushing metric:" + json + " of toplogy:" + toplogy_id + " to cluster:" + cluster_name);
+
+                if (!this.topic_ok.get()) {
+                    LOGGER.warn("topic:" + METRIC_KAFKA_TOPIC + " not ready,drop metric");
+                    ok = false;
+                } else {
+                    Arrays.asList(metric.get_componentMetric(), //
+                            metric.get_nettyMetric(),
+                            metric.get_streamMetric(),
+                            metric.get_taskMetric(),
+                            metric.get_topologyMetric(),
+                            metric.get_workerMetric()
+                    ).parallelStream().forEach((each_metric) -> {
+                        this.send(each_metric, info, metric_summary);
+                    });
+
+                    ok = true;
+                }
             } catch (Exception exception) {
-                LOGGER.error("fail to upload metric:" + metric_summary + " of topology:" + toplogy_id + " to cluster:" + cluster_name + " failed index:" + metric_cache_index);
+                LOGGER.error("fail to upload metric:" + metric_summary + " of topology:" + toplogy_id + " to cluster:" + cluster_name + " failed index:" + metric_cache_index, exception);
+                ok = false;
             } finally {
                 metric_context.markUploaded(metric_cache_index);
             }
+
+            return ok;
         });
+    }
+
+    protected void send(MetricInfo metric, TopologyMetricDataInfo info, Map<String, Object> metric_summary) {
+        metric.get_metrics().entrySet().parallelStream().forEach((entry) -> {
+            final String name = entry.getKey();
+            LOGGER.info("metric info" + info + " metric_summary:" + metric_summary);
+            entry.getValue().entrySet().parallelStream().forEach((window) -> {
+                int range = window.getKey();
+                MetricSnapshot snapshot = window.getValue();
+
+                String key = "";
+                String value = "";
+                //TODO
+
+                try {
+                    this.producer.get().send(new ProducerRecord<>(key, value));
+                } catch (InterruptedException | ExecutionException e) {
+                    LOGGER.warn("fail to send metric:" + name + " message,key:" + key + " value:" + value + " range:" + range + " snapshot:" + snapshot);
+                }
+            });
+
+        });
+    }
+
+    protected Map<String, Object> kafkaConfig() {
+        String kafka_servers = (String) this.metric_context.getNimbusData().getConf().get("kafka.servers");
+
+        Map<String, Object> configuration = new TreeMap<>();
+        configuration.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafka_servers);
+        configuration.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka_servers);
+        configuration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configuration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return configuration;
     }
 
     public static void main(String args[]) {
         try {
+            String[] zookeepers = new String[]{"10.202.77.200", "10.202.77.201", "10.202.77.202"};
             Map<String, Object> conf = Utils.readStormConfig();
-            conf.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList("10.202.77.200", "10.202.77.201", "10.202.77.202"));
+            conf.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList(zookeepers));
             conf.put(Config.STORM_ZOOKEEPER_PORT, 2181);
+            /*
             Nimbus.Client client = NimbusClient.getConfiguredClient(conf).getClient();
 
             TopologyMetric metric = new TopologyMetric();
@@ -130,7 +230,51 @@ public class JStromMetricKafkaUploader extends BaseMetricUploaderWithFlowControl
             metric.set_nettyMetric(metric_info);
 
             client.uploadTopologyMetrics("test-topplogy", metric);
-            LOGGER.info(client.getClusterInfo());
+            */
+
+            /*
+            ZkUtils zkutils = ZkUtils.apply(String.join(",", ((List<String>) conf.get(Config.STORM_ZOOKEEPER_SERVERS))),
+                    1000, 1000, false);
+
+            AdminUtils.createOrUpdateTopicPartitionAssignmentPathInZK(
+                    zkutils,
+                    METRIC_KAFKA_TOPIC,
+                    AdminUtils.assignReplicasToBrokers(
+                            AdminUtils.getBrokerMetadatas(
+                                    zkutils,
+                                    RackAwareMode.Safe$.MODULE$,
+                                    AdminUtils.getBrokerMetadatas$default$3()
+                            ),
+                            10,
+                            1,
+                            AdminUtils.assignReplicasToBrokers$default$4(),
+                            AdminUtils.assignReplicasToBrokers$default$5()
+                    ),
+                    new Properties(),
+                    true
+            );
+            */
+
+            Map<String, Object> configuration = new TreeMap<>();
+            configuration.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "10.202.77.200:9092");
+            configuration.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.202.77.200:9092");
+            configuration.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            configuration.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            configuration.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.202.77.200:9092");
+
+            KafkaProducer<String, String> producer = new KafkaProducer<>(configuration);
+            producer.send(new ProducerRecord<>(METRIC_KAFKA_TOPIC, "test-key", "1")).get();
+
+            //AdminUtils.createTopic(zkutils,"jstrom-metrics",10,1,new Properties(),RackAwareMode.Safe$.MODULE$);
+
+            //LOGGER.info(client.getClusterInfo());
+            /*
+            AdminClient.create(configuration)
+                    .createTopics(
+                            Arrays.asList(new NewTopic(METRIC_KAFKA_TOPIC, 10, (short) 1)),
+                            new CreateTopicsOptions()
+                    ).all().get();
+            */
         } catch (Exception e) {
             LOGGER.error("unexpected exception", e);
         } finally {
