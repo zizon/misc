e768a06cfa7d405d2e4e25dce9fe33e4b01163da
zizon <ZhiZhongQiu@sf-express.com>
2017-12-27 11:14:14 +0800

    refactor cache

---
+++ src/main/java/com/sf/misc/ReferenceBaseCache.java
@@ -0,0 +1,167 @@
+package com.sf.misc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+public class ReferenceBaseCache<VALUE_TYPE> {
+
+    private static final Log LOGGER = LogFactory.getLog(ReferenceBaseCache.class);
+
+    public static final ScheduledExecutorService POOL = Executors.newScheduledThreadPool(1);
+
+    protected static final Queue<WeakReference<ReferenceBaseCache<?>>> MANAGED = new ConcurrentLinkedQueue<>();
+
+    static {
+        pool().scheduleAtFixedRate(() -> {
+            LOGGER.info("trigger weak reference evication...");
+            long now = System.currentTimeMillis();
+            Iterator<WeakReference<ReferenceBaseCache<?>>> cache_iterator = MANAGED.iterator();
+            while (cache_iterator.hasNext()) {
+                WeakReference<ReferenceBaseCache<?>> reference = cache_iterator.next();
+
+                // remove garbage collected
+                ReferenceBaseCache<?> instance = reference.get();
+                if (instance == null) {
+                    cache_iterator.remove();
+                    continue;
+                }
+
+                Iterator<?> cache_entry_iterator = instance.holder.entrySet().iterator();
+                while (cache_entry_iterator.hasNext()) {
+                    @SuppressWarnings("unchecked")
+                    Map.Entry<String, Map.Entry<WeakReference, Long>> entry = (Map.Entry<String, Map.Entry<WeakReference, Long>>) cache_entry_iterator.next();
+
+                    Map.Entry<WeakReference, Long> value = entry.getValue();
+                    if (value.getKey().get() == null) {
+                        // remove garbage collected
+                        cache_entry_iterator.remove();
+                        LOGGER.info("remove garbage collected");
+                    } else if (now - value.getValue() >= 1000 * 60 * 60) {
+                        // remove last access one hour before
+                        cache_entry_iterator.remove();
+                        LOGGER.info("remove expired");
+                    }
+                }
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+
+    protected final String name;
+    protected ConcurrentMap<String, Map.Entry<Reference<VALUE_TYPE>, Long>> holder;
+
+    public ReferenceBaseCache(String name) {
+        this.holder = new ConcurrentHashMap<>();
+        this.name = name;
+
+        // register this
+        MANAGED.offer(new WeakReference<>(this));
+    }
+
+    public static ScheduledExecutorService pool() {
+        return POOL;
+    }
+
+    public VALUE_TYPE fetch(final String key, final Function<String, VALUE_TYPE> when_missing) {
+        // access time
+        long now = System.currentTimeMillis();
+
+        // once initializer
+        AtomicReference<VALUE_TYPE> missing_value = new AtomicReference<>(null);
+
+        // try fetch from cache
+        Map.Entry<Reference<VALUE_TYPE>, Long> entry = holder.get(key);
+        if (entry == null) {
+            LOGGER.info("cache:" + this.name + " miss key:" + key);
+            try {
+                entry = POOL.submit(() -> {
+                    Map.Entry<Reference<VALUE_TYPE>, Long> presented = this.holder.get(key);
+                    if (presented != null) {
+                        return presented;
+                    }
+
+                    // only initialize once at this call stack
+                    VALUE_TYPE value = missing_value.get();
+                    if (value == null) {
+                        value = when_missing.apply(key);
+                        missing_value.set(value);
+                    }
+                    return newEntry(value, now);
+                }).get();
+            } catch (InterruptedException | ExecutionException e) {
+                LOGGER.warn("fail to async initialize value for key:" + key);
+
+                VALUE_TYPE value = missing_value.get();
+                if (value == null) {
+                    value = when_missing.apply(key);
+                    missing_value.set(value);
+                }
+                entry = newEntry(value, now);
+            }
+
+            // setup cache
+            holder.put(key, entry);
+        }
+
+        // update access time
+        entry.setValue(now);
+
+        // try weak reference
+        VALUE_TYPE hit = entry.getKey().get();
+        if (hit != null) {
+            return hit;
+        }
+
+        // garbage collected
+        hit = missing_value.get();
+        if (hit == null) {
+            // create new
+            hit = when_missing.apply(key);
+        }
+
+        return hit;
+    }
+
+    protected Reference<VALUE_TYPE> newRerence(VALUE_TYPE value) {
+        return new SoftReference<>(value);
+    }
+
+    protected Map.Entry<Reference<VALUE_TYPE>, Long> newEntry(final VALUE_TYPE value, final Long access) {
+        return new Map.Entry<Reference<VALUE_TYPE>, Long>() {
+            private Reference<VALUE_TYPE> holder = newRerence(value);
+            private Long access_time = access;
+
+            @Override
+            public Reference<VALUE_TYPE> getKey() {
+                return holder;
+            }
+
+            @Override
+            public Long getValue() {
+                return this.access_time;
+            }
+
+            @Override
+            public Long setValue(Long value) {
+                Long old = this.access_time;
+                this.access_time = value;
+                return old;
+            }
+        };
+    }
+}
--- src/main/java/com/sf/misc/hive/jdbc/HTTPPasswdAuthenticationProvider.java
+++ src/main/java/com/sf/misc/hive/jdbc/HTTPPasswdAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package com.sf.misc.hive.jdbc;
 
+import com.sf.misc.ReferenceBaseCache;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -8,20 +9,9 @@ import org.apache.hive.service.auth.PasswdAuthenticationProvider;
 
 import javax.security.sasl.AuthenticationException;
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * to activate this for hive jdbc server,
@@ -35,49 +25,16 @@ public class HTTPPasswdAuthenticationProvider implements PasswdAuthenticationPro
 
     public static final String AUTH_SERVER_URL = "sf.com.http.auth.url";
 
-    public static final ScheduledExecutorService POOL = Executors.newScheduledThreadPool(1);
-    protected static ConcurrentLinkedQueue<WeakReference<HTTPPasswdAuthenticationProvider>> CLEANER = new ConcurrentLinkedQueue<>();
-
-    static {
-        POOL.scheduleAtFixedRate(() -> {
-            long now = System.currentTimeMillis();
-            Iterator<WeakReference<HTTPPasswdAuthenticationProvider>> iterator = CLEANER.iterator();
-            while (iterator.hasNext()) {
-                WeakReference<HTTPPasswdAuthenticationProvider> current = iterator.next();
-                HTTPPasswdAuthenticationProvider provider = current.get();
-
-                if (provider == null) {
-                    // collected
-                    iterator.remove();
-                    continue;
-                }
-
-                // now evict
-                Iterator<Map.Entry<String, Map.Entry<String, Long>>> cache_entrys = provider.cacheIterator();
-                while (cache_entrys.hasNext()) {
-                    Map.Entry<String, Map.Entry<String, Long>> entry = cache_entrys.next();
-
-                    // find access info
-                    Map.Entry<String, Long> access_info = entry.getValue();
-                    long then = access_info.getValue();
-                    if (now - then >= 1000 * 60) {
-                        cache_entrys.remove();
-                        LOGGER.info("expire user:" + entry.getKey());
-                    }
-                }
-            }
-        }, 0, 1, TimeUnit.MINUTES);
-    }
-
-
     protected Configuration conf;
-    protected ConcurrentMap<String, Map.Entry<String, Long>> caches;
+    protected ReferenceBaseCache<String> caches;
     protected String auth_url;
 
+    @SuppressWarnings("unused")
     public HTTPPasswdAuthenticationProvider(HiveConf conf) {
         this(conf, true);
     }
 
+    @SuppressWarnings("unused")
     public HTTPPasswdAuthenticationProvider(Configuration conf, boolean ignore) {
         super();
 
@@ -90,15 +47,11 @@ public class HTTPPasswdAuthenticationProvider implements PasswdAuthenticationPro
         }
 
         // setup cache
-        this.caches = new ConcurrentHashMap<>();
-
-        // register
-        CLEANER.offer(new WeakReference<>(this));
+        this.caches = new ReferenceBaseCache<>("http-user-password-authentication-cache");
     }
 
     @Override
     public void Authenticate(final String user, final String password) throws AuthenticationException {
-
         // sanity check
         if (user == null) {
             throw new AuthenticationException("no user for authentication");
@@ -106,71 +59,16 @@ public class HTTPPasswdAuthenticationProvider implements PasswdAuthenticationPro
             throw new AuthenticationException("no password provided for authentication ,of user:" + user);
         }
 
-
-        // find authentication cache
-        Map.Entry<String, Long> hit = this.caches.get(user);
-        if (hit == null) {
-            try {
-                Future<Boolean> ok = this.asyncValidate(user, password);
-                // a bit tricky,try if other had already kick cache up
-                hit = this.caches.get(user);
-                if (hit == null) {
-                    if (ok.get()) {
-                        hit = new Map.Entry<String, Long>() {
-                            private Long access_time = System.currentTimeMillis();
-                            private String saved_password = password;
-
-                            @Override
-                            public String getKey() {
-                                return this.saved_password;
-                            }
-
-                            @Override
-                            public Long getValue() {
-                                return access_time;
-                            }
-
-                            @Override
-                            public Long setValue(Long value) {
-                                Long old = this.access_time;
-                                this.access_time = value;
-                                return old;
-                            }
-                        };
-
-                        // cache it
-                        this.caches.put(user, hit);
-                    }
-                }
-            } catch (InterruptedException e) {
-                LOGGER.warn("interrupt when validating user:" + user, e);
-            } catch (ExecutionException e) {
-                LOGGER.error("unexpected execption when validating user:" + user, e);
+        String saved_password = this.caches.fetch(user, (user_name) -> {
+            if (this.validate(user_name, password)) {
+                return password;
             }
-        }
-
-        // no password info for user
-        if (hit == null) {
-            throw new AuthenticationException("password not match of user:" + user);
-        }
-
-        // renew cache
-        hit.setValue(System.currentTimeMillis());
-
-        // check
-        if (password.compareTo(hit.getKey()) != 0) {
-            throw new AuthenticationException("password not match of user:" + user);
-        }
-    }
-
-    protected ExecutorService pool() {
-        return POOL;
-    }
-
-    protected Future<Boolean> asyncValidate(final String user, final String password) {
-        return this.pool().submit(() -> {
-            return this.validate(user, password);
+            return null;
         });
+
+        if (saved_password == null || password.compareTo(saved_password) != 0) {
+            throw new AuthenticationException("password not match of user:" + user);
+        }
     }
 
     protected boolean validate(String user, String password) {
@@ -194,20 +92,22 @@ public class HTTPPasswdAuthenticationProvider implements PasswdAuthenticationPro
         return ok;
     }
 
-    protected Iterator<Map.Entry<String, Map.Entry<String, Long>>> cacheIterator() {
-        return this.caches.entrySet().iterator();
-    }
-
     public static void main(String[] args) {
         LOGGER.info("start");
-        try {
-            Configuration configuration = new Configuration();
-            configuration.set(AUTH_SERVER_URL, "10.202.77.200:6080");
-            HTTPPasswdAuthenticationProvider provider = new HTTPPasswdAuthenticationProvider(configuration, true);
-            provider.Authenticate("hive", "hive1");
-        } catch (AuthenticationException e) {
-            LOGGER.error("unexpected failure", e);
+
+        Configuration configuration = new Configuration();
+        configuration.set(AUTH_SERVER_URL, "10.202.77.200:6080");
+        HTTPPasswdAuthenticationProvider provider = new HTTPPasswdAuthenticationProvider(configuration, true);
+        for (int i = 0; i < 100; i++) {
+            try {
+                provider.Authenticate("hive", "hive1");
+                LOGGER.info("auth ok");
+            } catch (AuthenticationException e) {
+                //LOGGER.error("unexpected failure", e);
+            }
         }
+
+        LockSupport.park();
     }
 
 }
