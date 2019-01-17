c344e820becef03bbbf4253744ac6e9501c26ac7
zizon <ZhiZhongQiu@sf-express.com>
2019-01-17 10:32:45 +0800

    add build script

---
+++ antman/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+current=`dirname "${BASH_SOURCE-$0}"`
+old=`pwd`
+
+cd $current
+
+#mvn package -DskipTests=true
+
+dist="$current/target/dist"
+
+rm -rf $dist
+mkdir -p $dist
+
+for jar in `find $current/target/ | grep jar$`;do
+    cp -f $jar $dist
+done
+
+cp $current/start.sh $dist/ 
+cp $current/log4j2.properties $dist/
+
+cd $old
+
